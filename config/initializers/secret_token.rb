2a98f89dcfec4f408391663920fa2ec6a11e9f81
Vladimir Aderikhin <vladimirbisn@gmail.com>
2015-01-19 16:03:13 +0300

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Retrofit2::Application.config.secret_token = 'ccdb10a863a3f6d9a999c3cf312293ac7b5d98c223cd04aedd483086ddb884152c0c7d2a740123e44c5b2ce93997f675d6df252673879f1855edf37a16a0fcbc'
